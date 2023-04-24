43626b1c57361ad8ddbcf9fbdc7d300fa5bdaf2b
Richard Sivila <e18184.com>
2023-04-24 17:04:50 -0400

    24-4-2023

---
+++ src/Gui00.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+
+public class Gui00 extends JFrame {
+    // Constantes y componentes (objetos)
+    public Gui00() {
+        super("Ejemplo 00");
+        // Configurar Componentes ;
+        // Configurar Manejadores Eventos ;
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    } // Terminar la aplicación al cerrar la ventana.
+
+    public static void main(String args[]) {
+        Gui00 aplicacion = new Gui00();
+    }
+}
--- src/grafico.java
+++ src/grafico.java
@@ -1,11 +1,23 @@
 import javax.swing.*;
+import java.awt.Font;
 
 public class grafico {
     public static void main(String args[]) {
         JFrame f = new JFrame("Ejemplo");
-        f.setSize(100, 100);
-        JLabel l = new JLabel("Hola");
+
+        f.setSize(500, 500);
+        // 1024 x 768
+        f.setLocation(1024 / 2, 768 / 2);
+        JLabel l = new JLabel();
+        Font fuente = new Font("Serif", Font.ITALIC, 50);
+
+        l.setFont(fuente);
+
+        l.setText(("tamano texto:" + l.getFont().getSize() + " nombre de la fuente: " +
+                l.getFont().getFontName()));
+
         f.getContentPane().add(l);
+
         f.show();
         try {
             System.in.read();
